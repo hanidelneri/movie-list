fc05482d616d11e5354418d8d9c5d9e37b8847b6
Hanie <h7.gharedaghi@gmail.com>
2021-01-22 18:52:05 +0330

    adding input component

---
+++ src/components/core/input/component.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-text-field
+    v-model="innerValue"
+    clearable
+    outlined
+    append-icon="mdi-magnify"
+    v-bind="$attrs"
+  />
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      innerValue: ""
+    };
+  },
+  props: {
+    value: String
+  },
+  watch: {
+    innerValue: {
+      handler(newValue) {
+        this.innerValue = newValue;
+        this.$emit("input", newValue);
+      }
+    }
+  },
+  mounted() {
+    this.innerValue = this.value;
+  }
+});
+</script>
