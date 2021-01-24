7a520534fe2dca193b22a83f1b82b0025d7315c9
Hanie <h7.gharedaghi@gmail.com>
2021-01-24 20:03:55 +0330

    removing vuex

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueCompositionApi from "@vue/composition-api";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.use(VueCompositionApi);
@@ -10,7 +9,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
