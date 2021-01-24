73b43fb46802557c2cd65c03db67fc264fd48e47
Hanie <h7.gharedaghi@gmail.com>
2021-01-24 20:01:35 +0330

    adding favourites route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import Favourites from "../views/Favourites.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
+    path: "/favourites",
+    name: "Favourites",
+    component: Favourites
+  },
+  {
     path: "/",
     name: "Home",
     component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
